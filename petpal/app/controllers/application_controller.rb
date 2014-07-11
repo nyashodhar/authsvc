e0797a6a7f694f54a4cdef87eb2cd3dc7470db22
nyashodhar <n.yashodhar@gmail.com>
2014-07-10 21:24:43 -0700

    Per-Yash-Will

--- petpal/app/controllers/application_controller.rb
+++ petpal/app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exceptions Per
+  # Prevent CSRF attacks by raising an exceptions Per-Yash-Will
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
