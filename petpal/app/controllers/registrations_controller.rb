c1bc8ab3413bdb23fb60a8b4709502a3c492e743
Per Blix <per.blix@mac.com>
2014-07-20 12:01:04 -0700

    Added PUT /user/editUser.json and refactored token authentication code

--- petpal/app/controllers/registrations_controller.rb
+++ petpal/app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@
 #
 # TODO: Implement and test this
 # Update user
-# PUT /user/register/edit
+# PUT /user/register
 # curl -v -X PUT http://127.0.0.1:3000/user/register.json -H "Content-Type: application/json" -d '{"user":{"email":"test@example.com", "password":"Test1234", "password_confirmation":"Test1234"}}'
 #
 # TODO: Implement a delete request handler
@@ -20,10 +20,8 @@
 
 class RegistrationsController < Devise::RegistrationsController
 
-  #prepend_before_filter :authenticate_scope!, only: [:foo]
-
   # Required to for devise to not require the presence of an authenticity token
-  skip_before_action :verify_authenticity_token, :only => :create
+  skip_before_action :verify_authenticity_token, :only => [:create, :update, :editUser]
 
   respond_to :json
 
@@ -33,20 +31,44 @@ class RegistrationsController < Devise::RegistrationsController
   # curl -v -X GET http://127.0.0.1:3000/user/lookup -H "X-User-Token: a6XK1qPfwyNd_HqjsgSS"
   #################
   def lookup
-    token = request.headers['X-User-Token']
-    userInfo = User.select("id, email").where("authentication_token=?", token).limit(1)
-
-    if(userInfo == nil)
-      render :status => 404, :json => I18n.t("token_verification_failed")
-    else
+    userInfo = verifyToken(request)
+    if(!userInfo.blank?)
       # TODO: Refresh the expiration time of the token
-      render :status => 200, :json => userInfo[0]
+      render :status => 200, :json => userInfo
     end
   end
 
-  # TODO: Clean up this stuff
-  def foo
+  ##################
+  # Update user
+  # PUT /user/register
+  # curl -v -X PUT http://127.0.0.1:3000/user/register.json -H "X-User-Token: a6XK1qPfwyNd_HqjsgSS" -H "Content-Type: application/json" -d '{"user":{"email":"test@example.com", "password":"Test1234", "password_confirmation":"Test1234"}}'
+  ##################
+  def editUser
 
+    STDOUT.write "UPDATE: HELLO\n"
+
+    userInfo = verifyToken(request)
+    if(!userInfo.blank?)
+      respond_to do |format|
+        format.json {
+
+          #STDOUT.write "UPDATE: authenticated userInfo.id: #{userInfo.id}\n"
+          user = User.find_by_id(userInfo.id)
+          #STDOUT.write "UPDATE: user before update: #{user.inspect}\n"
+
+          ##
+          ## TODO: Currently the password can be update without specifying the old
+          ## password and verifying that the old password 'checks out'
+          ##
+
+          update_resource(user, account_update_params)
+
+          #userAfter = User.find_by_id(userInfo.id)
+          #STDOUT.write "UPDATE: user after update: #{user.inspect}\n"
+        }
+      end
+      render :status => 200, :json => userInfo
+    end
   end
 
   ##################
@@ -65,8 +87,23 @@ class RegistrationsController < Devise::RegistrationsController
 	        	render :json => resource.errors, :status => :unprocessable_entity
 		    end
 	    }
-	end
+	  end
+  end
 
+  private
+
+  ##################
+  # Sends a 403 JSON response if the auth token is not valid.
+  # Other methods must check if returned userInfo is blank before proceeding with their processing
+  ##################
+  def verifyToken(request)
+
+    token = request.headers['X-User-Token']
+    userInfo = User.select("id, email").where("authentication_token=?", token).limit(1)
+    if(userInfo.blank?)
+      render :status => 403, :json => I18n.t("token_verification_failed")
+    end
+    return userInfo[0]
   end
 
 end
--- petpal/config/routes.rb
+++ petpal/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'user/token/verify', to: 'sessions#verify', as: 'verify'
     get 'user/lookup', to: 'registrations#lookup', as: 'lookup'
+    put 'user/editUser', to: 'registrations#editUser', as: 'editUser'
   end
 
   devise_for :users, path: "user", path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }, :controllers => {:registrations => "registrations", :sessions => "sessions" }
