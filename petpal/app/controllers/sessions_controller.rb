16d76172924a98ec2ee3df73d32240ff44a9697b
Per Blix <per.blix@mac.com>
2014-07-27 20:13:03 -0700

    Completed ensureLoggedInAndAuthTokenNotExpired, ensures proper login check for all protected APIs

--- petpal/app/controllers/sessions_controller.rb
+++ petpal/app/controllers/sessions_controller.rb
@@ -22,48 +22,34 @@ class SessionsController < Devise::SessionsController
 
   respond_to :json
 
+  #
+  # Note: Check if there is a sign-in for the auth token, and that the sign-in
+  # is not expired  #
+  #
+  before_action :ensureLoggedInAndAuthTokenNotExpired, :only => [:verify]
+
   ################
   # Verify token
   # GET /user/token/verify
-  # curl -X GET http://127.0.0.1:3000/user/token/verify -H "X-User-Token: eXcPsqL-19yzxVpYceeF"
+  # curl -X GET http://127.0.0.1:3000/user/token/verify -H "X-User-Token: GcZy__QhxcxFvdqgpTtz"
   ################
   def verify
-
-    STDOUT.write "SessionsController#verify: HELLO THERE\n"
-    STDOUT.write "current_user = #{current_user.inspect}\n"
-    STDOUT.write "session[:userId] = #{session[:userId]}\n"
-    STDOUT.write "session = #{session.inspect}\n"
-
-    token = request.headers['X-User-Token']
-
-    # Note: This method is in ApplicationHelper
     userInfo = getLoggedInUser(request)
-
-    # Note: This method is in ApplicationHelper
-    renderAuthResponse(userInfo)
-
-    #if(userInfo.blank?)
-    #  render :status => 403, :json => I18n.t("token_verification_failed")
-    #else
-    #  # TODO: Refresh the expiration time of the token
-    #  render :status => 200, :json => userInfo[0]
-    #end
+    render :status => 200, :json => userInfo
   end
-
-  ################
-  # Sign in:
-  # POST /user/login
-  # curl -X POST http://127.0.0.1:3000/user/login.json -H "Content-Type: application/json" -d '{"user":{"email":"test@example.com", "password":"Test1234"}}'
-  ################
 
   #
   # Note: Before a sign-in, we need to check if the current token is expired
   # If it's expired, we need to make sure it's reset. Otherwise the user will
   # be allowed to continue to use the auth token even after it has expired!
   #
-
   before_action :clearStaleTokenBeforeSignIn, :only => [:create]
 
+  ################
+  # Sign in:
+  # POST /user/login
+  # curl -X POST http://127.0.0.1:3000/user/login.json -H "Content-Type: application/json" -d '{"user":{"email":"test@example.com", "password":"Test1234"}}'
+  ################
   def create
     super
   end
