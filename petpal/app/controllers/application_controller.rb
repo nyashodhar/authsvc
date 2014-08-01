786577c36c11f228352687dc32661edae7d8959c
Per Blix <per.blix@mac.com>
2014-08-01 15:46:23 -0700

    Limit stacktrace lenght on 500 errors, and ensure response is JSON

--- petpal/app/controllers/application_controller.rb
+++ petpal/app/controllers/application_controller.rb
@@ -6,4 +6,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exceptions Per-Yash-Will
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from StandardError do |e|
+    error(e)
+  end
+
+  def error(e)
+    trace = e.backtrace[0,10].join("\n")
+    logger.error "Custom error handler - Error: #{e.class.name} : #{e.message}, Trace: #{trace}\n"
+    render :status => 500, :json => {:error => I18n.t("500response_internal_server_error")}.to_json
+  end
+
 end
