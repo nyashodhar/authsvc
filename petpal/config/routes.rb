7421530ebd3f852ca0970bb26b9bf550f0e34cba
Per Blix <per.blix@mac.com>
2014-08-06 23:23:58 -0700

    Remove some old comments in routes

--- petpal/config/routes.rb
+++ petpal/config/routes.rb
@@ -25,12 +25,7 @@ Rails.application.routes.draw do
     match "*path", to: "errors#not_found", via: :all
   end
 
-  #devise_for :users, :controllers => {:registrations => "registrations", :sessions => "sessions" }
   devise_for :users
-
-  #match "*path", to: "errors#not_found", via: :all
-
-  ##devise_for :users, :controllers => {:registrations => "registrations"}
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
