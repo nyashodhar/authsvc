8722572d2226b5fd5ccc7ab0c092dc3d75237790
Per Blix <per.blix@mac.com>
2014-08-14 21:08:11 -0700

    Configure mandrill mailchimp in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,19 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   #####################################################################################
 
+  config.action_mailer.default_url_options = {
+      :host => 'authpetpalci.herokuapp.com'
+  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :port => '587',
+      :address => 'smtp.mandrillapp.com',
+      :user_name => ENV['MANDRILL_USERNAME'],
+      :password => ENV['MANDRILL_APIKEY'],
+      :domain => 'heroku.com',
+      :authentication => :plain
+  }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
